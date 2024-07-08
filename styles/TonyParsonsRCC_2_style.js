83363583bb7e6227646d459eb2712de213ad959e
lucasnorvid <lucas.norvid@transitionscare.com>
2024-07-08 15:15:21 -0500

    Update TonyParsonsRCC_2_style.js

--- styles/TonyParsonsRCC_2_style.js
+++ styles/TonyParsonsRCC_2_style.js
@@ -21,7 +21,7 @@ var style_TonyParsonsRCC_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(114,155,111,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(114,155,111,0.6)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
